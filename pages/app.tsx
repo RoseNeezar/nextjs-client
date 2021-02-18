3e82d3a6dbad8565c56a8438e60a39111e5bb247
Neezar <rforlyffe@gmaicl.com>
2021-02-18 14:46:48 +0800

    cleanup

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,20 +1,10 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import NextLink from "next/link";
 import Main from "../app/PageComponent/Main";
 import About from "../app/PageComponent/About";
 
 const App = () => {
-  let history = useHistory();
-  function handleClick() {
-    history.push("/about");
-  }
   return (
     <Router basename="/app">
       <div>
